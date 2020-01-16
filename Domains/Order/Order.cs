dff112579a1aa90739900bea6364a7eb67c5166d
William Bassalobre <william.bassalobre@objective.com.br>
2020-01-16 18:38:29 -0300

    First commit.

---
+++ Domains/Order/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marketplace.Domains.Order
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public OrderStatus Status { get; set; }
+
+    }
+}
---
+++ Domains/Order/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marketplace.Domains.Order
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Devivered = 3
+    }
+}
